fc33f4e44b0ae738f2763ba259ba743e59f6da59
R K Chitra <chitra31399@gmail.com>
2021-07-09 12:02:51 +0530

    Asynchronous Programming

--- app.js
+++ app.js
@@ -2,4 +2,25 @@ const fs = require('fs')
 const fileName = "target.txt"
 
 //watches out for changes; 2 args : filename & callback function 
-fs.watch(fileName,() => console.log("File changed"))
+//handle error; else the program terminates 
+fs.readFile(fileName, (err, data) => {
+	if(err)
+	{
+		console.log(err);
+	}
+	else
+	{
+		console.log(data.toString());
+	}
+})
+console.log('Node JS Async Programming');
+
+
+/*
+
+Node JS takes the events and executes line by line or one by one. However, some events take longer than
+others. Put both into event loop, realized the first one was taking too much time and left it to the 
+callback function. It went ahead and executed the other event. Then the callback method returned the data
+and then executed that. After this, it waits for more events. 
+
+*/
