bece99343e1be6fab76df8997bacae219ccac352
R K Chitra <chitra31399@gmail.com>
2021-07-09 11:13:49 +0530

    programming for event loop

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-const express = require('express')
-const app = express() //executed the express app; it's now ready to listen to the incoming request 
-//the properties and methods of the express package can be accessed using the app variable 
+const fs = require('fs')
+const fileName = "target.txt"
 
-
-//listen to incoming request from browser
-app.get('/',(req, res) => {
-	res.send("Hello from express!")
-})
-
-app.listen(3000)
+//watches out for changes; 2 args : filename & callback function 
+fs.watch(fileName,() => console.log("File changed"))
