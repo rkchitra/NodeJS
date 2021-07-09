6015ef8764ecd2af935ab20e54e2b6dd8560b1d6
R K Chitra <chitra31399@gmail.com>
2021-07-09 12:11:52 +0530

    Synchronous Programming

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
 const fs = require('fs')
 const fileName = "target.txt"
 
-//watches out for changes; 2 args : filename & callback function 
-//handle error; else the program terminates 
-fs.readFile(fileName, (err, data) => {
-	if(err)
-	{
-		console.log(err);
-	}
-	else
-	{
-		console.log(data.toString());
-	}
-})
-console.log('Node JS Async Programming');
+const data = fs.readFileSync(fileName) //synchronous model; blocking;
+console.log(data.toString())
 
-
-/*
-
-Node JS takes the events and executes line by line or one by one. However, some events take longer than
-others. Put both into event loop, realized the first one was taking too much time and left it to the 
-callback function. It went ahead and executed the other event. Then the callback method returned the data
-and then executed that. After this, it waits for more events. 
-
-*/
+console.log("\nNode JS Synchronous Programming ")
